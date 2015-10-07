1b6a790c2bb66f50976b9594f343014528edd45b
diamant1991 <diamant1988@mail.ru>
2015-10-07 10:16:36 +0300

    add mask inputs

--- js/script.js
+++ js/script.js
@@ -13,6 +13,24 @@ $(document).ready(function(){
       }     
       return false;
     });
+
+    $('.email-input').inputmask({
+      mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+      greedy: false,
+      onBeforePaste: function (pastedValue, opts) {
+          pastedValue = pastedValue.toLowerCase();
+          return pastedValue.replace("mailto:", "");
+      },
+      definitions: {
+          '*': {
+              validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+              cardinality: 1,
+              casing: "lower"
+          }
+      }
+  });
+
+
 });
 
 // Select
@@ -131,6 +149,4 @@ function checkSection(){
    }
   })
 }
-
-
 
